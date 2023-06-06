5669025a0e5f24bfdabd643860b9b453f91e7d23
DennisCatana <dennis.joel235@outlook.es>
2023-06-06 09:04:57 -0500

    Constructor, Setters and Getters

--- src/Fijuras3D.java
+++ src/Fijuras3D.java
@@ -1,2 +1,57 @@
 public class Fijuras3D {
+    float area;
+    float base;
+    float radio;
+    float altura;
+    float lado;
+
+    public Fijuras3D(float area, float base, float radio, float altura, float lado) {
+        this.area = area;
+        this.base = base;
+        this.radio = radio;
+        this.altura = altura;
+        this.lado = lado;
+    }
+
+    public float getArea() {
+        return area;
+    }
+
+    public void setArea(float area) {
+        this.area = area;
+    }
+
+    public float getBase() {
+        return base;
+    }
+
+    public void setBase(float base) {
+        this.base = base;
+    }
+
+    public float getRadio() {
+        return radio;
+    }
+
+    public void setRadio(float radio) {
+        this.radio = radio;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getLado() {
+        return lado;
+    }
+
+    public void setLado(float lado) {
+        this.lado = lado;
+    }
 }
+
+
